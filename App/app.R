d3a3e90cdc99d5690db9f63581ddd12743726579
Teresa Rokos <teresarokos@Teresas-MacBook-Air.local>
2018-11-13 21:31:16 -0500

    "fixed my app and got it working!!"

--- App/app.R
+++ App/app.R
@@ -24,19 +24,19 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput(inputId = "service",
                   label = "Select a service:",
-                  choices = c("Lab Tests" = "this_visit_did_p_have_lab_tests",
-                              "Sonogram or Ultrasound" = "this_visit_did_p_have_sonogram_or_ultrsd",
-                              "X-Rays" = "this_visit_did_p_have_x_rays",
-                              "Mammogram" = "this_visit_did_p_have_a_mammogram",
-                              "MRI or CTscan" = "this_visit_did_p_have_an_mri_catscan",
-                              "EKG or ECG" = "this_visit_did_p_have_an_ekg_or_ecg", 
-                              "EEG" = "this_visit_did_p_have_an_eeg", 
-                              "Vaccination" = "this_visit_did_p_receive_a_vaccination", 
-                              "Anesthesia" = "this_visit_did_p_receive_anesthesia", 
-                              "Throat Swab" = "this_visit_did_p_have_a_throat_swab",
-                              "Other Diagnostic Test/Exam" = "this_visit_did_p_have_oth_diag_test_exam", 
-                              "Surgery" = "was_surg_proc_performed_on_p_this_visit", 
-                              "Prescribed Medication" = "any_medicine_prescribed_for_p_this_visit"))
+                  choices = c("Lab Tests",
+                              "Sonogram or Ultrasound",
+                              "X-Rays",
+                              "Mammogram",
+                              "MRI or CTscan",
+                              "EKG or ECG", 
+                              "EEG", 
+                              "Vaccination", 
+                              "Anesthesia", 
+                              "Throat Swab",
+                              "Other Diagnostic Test/Exam", 
+                              "Surgery", 
+                              "Prescribed Medication"))
     ),
     
     # MY MAIN PANEL
@@ -51,20 +51,31 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  # Generate a reactive subset of the data to filter only for observations where the selected service was provided. 
-  service_subset <- reactive({
-    req(input$service)
-    filter(app_data, input$service == "1 YES")
-  })
-  
   # The reactive subset does not appear to be working, so creating a test one for the sake of a graphic.
   service_subset_test <- filter(app_data, this_visit_did_p_have_lab_tests == "1 YES")
   
-  # Creating a plot using the non-reactive subset of data created directly above.
+  # Creating a plot using a subset of the data filtered by the service the user selects.
   output$histPlot <- renderPlot({
     
+    # Filtering the data based on the selected service. 
+    service_subset <- switch(input$service,
+                             "Lab Tests" = app_data %>% filter(this_visit_did_p_have_lab_tests == "1 YES"),
+                             "Sonogram or Ultrasound" = app_data %>% filter(this_visit_did_p_have_sonogram_or_ultrsd == "1 YES"),
+                             "X-Rays" = app_data %>% filter(this_visit_did_p_have_x_rays == "1 YES"),
+                             "Mammogram" = app_data %>% filter(this_visit_did_p_have_a_mammogram == "1 YES"),
+                             "MRI or CTscan" = app_data %>% filter(this_visit_did_p_have_an_mri_catscan == "1 YES"),
+                             "EKG or ECG" = app_data %>% filter(this_visit_did_p_have_an_ekg_or_ecg == "1 YES"), 
+                             "EEG" = app_data %>% filter(this_visit_did_p_have_an_eeg == "1 YES"), 
+                             "Vaccination" = app_data %>% filter(this_visit_did_p_receive_a_vaccination == "1 YES"), 
+                             "Anesthesia" = app_data %>% filter(this_visit_did_p_receive_anesthesia == "1 YES"), 
+                             "Throat Swab" = app_data %>% filter(this_visit_did_p_have_a_throat_swab == "1 YES"),
+                             "Other Diagnostic Test/Exam" = app_data %>% filter(this_visit_did_p_have_oth_diag_test_exam == "1 YES"), 
+                             "Surgery" = app_data %>% filter(was_surg_proc_performed_on_p_this_visit == "1 YES"), 
+                             "Prescribed Medication" = app_data %>% filter(any_medicine_prescribed_for_p_this_visit == "1 YES")
+    )
+    
     # MY PLOT
-    ggplot(data = service_subset_test, aes(x = tot_exp_for_event_erfxp16x_erdxp16x)) + 
+    ggplot(data = service_subset, aes(x = tot_exp_for_event_erfxp16x_erdxp16x)) + 
       geom_histogram(bins = 30) + 
       scale_x_continuous(name = "Total Expenditure for Event", 
                          breaks = c(100, 1000, 10000, 100000),
@@ -77,7 +88,23 @@ server <- function(input, output) {
   
   # This demonstrates whether or not my data is reading in properly (the reactive subset does not work).
   output$n <- renderUI({
-    paste0("There were a total of ", nrow(service_subset_test), " emergency visits which provided this service.")
+    service_subset <- switch(input$service,
+                             "Lab Tests" = app_data %>% filter(this_visit_did_p_have_lab_tests == "1 YES"),
+                             "Sonogram or Ultrasound" = app_data %>% filter(this_visit_did_p_have_sonogram_or_ultrsd == "1 YES"),
+                             "X-Rays" = app_data %>% filter(this_visit_did_p_have_x_rays == "1 YES"),
+                             "Mammogram" = app_data %>% filter(this_visit_did_p_have_a_mammogram == "1 YES"),
+                             "MRI or CTscan" = app_data %>% filter(this_visit_did_p_have_an_mri_catscan == "1 YES"),
+                             "EKG or ECG" = app_data %>% filter(this_visit_did_p_have_an_ekg_or_ecg == "1 YES"), 
+                             "EEG" = app_data %>% filter(this_visit_did_p_have_an_eeg == "1 YES"), 
+                             "Vaccination" = app_data %>% filter(this_visit_did_p_receive_a_vaccination == "1 YES"), 
+                             "Anesthesia" = app_data %>% filter(this_visit_did_p_receive_anesthesia == "1 YES"), 
+                             "Throat Swab" = app_data %>% filter(this_visit_did_p_have_a_throat_swab == "1 YES"),
+                             "Other Diagnostic Test/Exam" = app_data %>% filter(this_visit_did_p_have_oth_diag_test_exam == "1 YES"), 
+                             "Surgery" = app_data %>% filter(was_surg_proc_performed_on_p_this_visit == "1 YES"), 
+                             "Prescribed Medication" = app_data %>% filter(any_medicine_prescribed_for_p_this_visit == "1 YES")
+    )
+    
+    paste0("There were a total of ", nrow(service_subset), " emergency visits surveyed that provided this service.")
   })
 }
 
